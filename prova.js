ba726dd10d3817f825062bd59a79e402d5d74903
Pittularo <34223786+Pittularo@users.noreply.github.com>
2022-12-14 18:22:02 +0100

    Mainly la tendina finalmente clonabile

--- prova.js
+++ prova.js
@@ -1,14 +1,1 @@
-const object1 = {
-  a: 'somestring',
-  b: 42,
-  c: false
-};
-
-//Object.values(object1).forEach(x=>{console.log('ciao ' + x)});
-
-let text = ""
-for (let i = 0; i < 5; i++) {
-  text = "The number is " +(Object.values(object1).length - i);
-  console.log(text)
-}
-
+console.log(4%3)
